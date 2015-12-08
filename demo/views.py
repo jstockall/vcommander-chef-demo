6191ccb720b36d38435df617795a953d6fde6780
Jesse Stockall <jstockall@embotics.com>
2015-12-07 20:02:39 -0500

    Add demo module

---
+++ demo/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.template import RequestContext, Template
+from django.http import HttpResponse
+from django.db import connection
+
+def index(request):
+    cursor = connection.cursor()
+    cursor.execute("SELECT now();")
+    dbtime = cursor.fetchone()[0]
+    response = HttpResponse()
+    response.write("<head><title>Embotics vCommander Demo: <%= node.name %></title></head><p>")
+    html = Template('<img src="{{ STATIC_URL }}chef.png" />')
+    response.write(html.render(RequestContext(request)))
+    html = Template('<img src="{{ STATIC_URL }}vmware.jpg" />')
+    response.write(html.render(RequestContext(request)))
+    response.write("</p><br><h1>Welcome to the Embotics Chef Demo Server!<h1>")
+    response.write("<h2><%= node.cloud.public_hostname %> on <%= node.cloud.public_ipv4 %><br><br>")
+    response.write("MySQL connected on <%= @db.cloud.public_hostname %>:<%= @db['mysql']['port'] %><br><br>")
+    response.write("Last updated by Chef at <%= Time.now %><br><br>")
+    response.write("Database time:")
+    response.write(dbtime)
+    response.write("</h2>")
+    return response
